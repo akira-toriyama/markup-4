6d9bf10fbd0332b5751d099165cecad80af28ceb
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-08-29 00:09:32 +0900

    :heavy_plus_sign:[x.x.o](_): Add SCSS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx"],
   compiler: {
     reactRemoveProperties: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };

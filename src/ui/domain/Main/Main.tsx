5a5676ff119acbe27340bc9b14e7b5c1f334f748
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-01 12:49:09 +0900

    :alembic:[x.x.o](_): Add expected error style

--- src/ui/domain/Main/Main.tsx
+++ src/ui/domain/Main/Main.tsx
@@ -3,6 +3,10 @@ import styles from "./main.module.scss";
 export const Main: React.FC = () => (
   <>
     <header className={styles.header}>h</header>
+
+    {/* CIエラーきぼんぬ */}
+    <div className={styles.div}>h</div>
+
     <main>m</main>
     <footer></footer>
   </>

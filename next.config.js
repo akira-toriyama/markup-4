9adaf360d9a65ca19f609b101033c962ca84102d
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-02 13:04:25 +0900

    :wrench:[x.x.o](_): Fix next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx"],
   compiler: {
     reactRemoveProperties: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
   },
 };

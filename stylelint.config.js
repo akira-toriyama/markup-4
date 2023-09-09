52a252e5586c096870f36677bacdc18b0d40a2ee
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-09 12:39:47 +0900

    :rotating_light:[x.x.x](main): Fix style

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,4 +3,7 @@ module.exports = {
   plugins: ["stylelint-prettier"],
   customSyntax: "postcss-scss",
   extends: ["stylelint-config-standard", "stylelint-config-css-modules"],
+  rules: {
+    "selector-class-pattern": null,
+  },
 };
